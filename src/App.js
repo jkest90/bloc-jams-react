afc0a65be3a8df9cf4f90c05daaf21af0cd71d7e
jkest90 <jkest618@gmail.com>
2018-03-31 17:22:31 -0600

    checkpoint 3.3: complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="App">
+            <header>
+               <h1>Bloc Jams</h1>
+                  <nav>
+                     <Link to='/'>Landing</Link>
+                     <Link to='/library'>Library</Link>
+                  </nav>
+            </header>
+            <main>
+               <Route exact path="/" component={Landing} />
+               <Route path="/library" component={Library} />
+            </main>
+         </div>
+      );
+   }
 }
 
 export default App;
