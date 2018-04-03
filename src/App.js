8d39cc6814331063fb97195519439f4f75af5339
jkest90 <jkest618@gmail.com>
2018-04-02 22:11:11 -0600

    add minor comments

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import Album from './components/Album';
 class App extends Component {
    render() {
       return (
+         // <Link > is a React component like Nav, except it links to a specific component using 'to' instead of 'href'.
+         // <Route /> is React component that displays when we navigate to the specific path. It then renders the component that we specify.  
          <div className="App">
             <header>
                <h1>Bloc Jams</h1>
-                  <nav>
-                     <Link to="/">Landing</Link>
-                     <Link to="/library">Library</Link>
-                  </nav>
+               <nav>
+                  <Link to="/">Landing</Link>
+                  <Link to="/library">Library</Link>
+               </nav>
             </header>
             <main>
                <Route exact path="/" component={Landing} />
