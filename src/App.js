faf4e21921f43d7ada41061376a9d1a207161b60
jkest90 <jkest618@gmail.com>
2018-04-04 17:59:21 -0600

    checkpoint 3.5: complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
@@ -9,7 +9,7 @@ class App extends Component {
    render() {
       return (
          // <Link > is a React component like Nav, except it links to a specific component using 'to' instead of 'href'.
-         // <Route /> is React component that displays when we navigate to the specific path. It then renders the component that we specify.  
+         // <Route /> is React component that displays when we navigate to the specific path. It then renders the component that we specify.
          <div className="App">
             <header>
                <h1>Bloc Jams</h1>
@@ -21,7 +21,7 @@ class App extends Component {
             <main>
                <Route exact path="/" component={Landing} />
                <Route path="/library" component={Library} />
-               <Route path="/album" component={Album} />
+               <Route path="/album/:slug" component={Album} />
             </main>
          </div>
       );
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
 
-const Library = () => (
-   <section className="library">
-      Library will go here.
-   </section>
-)
+class Library extends Component {
+   constructor(props) {
+      super(props);
+      this.state = { albums: albumData };
+   }
+
+
+// 1) Declare the route in App.js using the URL parameter 'path=/album/:slug'
+// 2) Map through the albumData array and use Link to pass in album.slug as our URL parameter.
+// 3) When we click on either album, it uses this.state.albums.slug as the new URL parameter, taking us to a new page.
+
+   render() {
+      return (
+         <section className='library'>
+            {
+               this.state.albums.map( (album, index) =>
+                  <Link to={`/album/${album.slug}`} key={index}>
+                     <img src={album.albumCover} alt={album.title} />
+                     <div>{album.title}</div>
+                     <div>{album.artist}</div>
+                     <div>{album.songs.length} songs</div>
+                  </Link>
+               )
+            }
+         </section>
+      );
+   }
+}
 
 export default Library;
